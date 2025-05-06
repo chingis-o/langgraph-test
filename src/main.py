35802edc8f4522fa287fb90f93fcf75e615474a0
Chingis Ochirov <chingis09@gmail.com>
2025-05-06 21:27:16 +0800

    basic state

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 import getpass
 import os
+_set_env("ANTHROPIC_API_KEY")
+
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
 
 
 def _set_env(var: str):
     if not os.environ.get(var):
         os.environ[var] = getpass.getpass(f"{var}: ")
 
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
 
-_set_env("ANTHROPIC_API_KEY")
+
+graph_builder = StateGraph(State)
